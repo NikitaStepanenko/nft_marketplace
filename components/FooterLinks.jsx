66ff79d15f599c8eab5f353d2db1fd540a15524d
NikitaStepanenko <stepanenkonikit@gmail.com>
2022-08-09 16:19:14 +0300

    Complete footer

---
+++ components/FooterLinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FooterLinks = ({ heading, items }) => (
+  <div className="flex-1 justify-start items-start">
+    <h3 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl md-10">{heading}</h3>
+    {items.map((item, index) => (
+      <p key={index} className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3">{item}</p>
+    ))}
+  </div>
+);
+
+export default FooterLinks;
--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer';
 import MenuItems from './MenuItems';
 import Button from './Button';
 import ButtonGroup from './ButtonGroup';
+import FooterLinks from './FooterLinks';
 
-export { Navbar, Footer, MenuItems, Button, ButtonGroup };
+export { Navbar, Footer, MenuItems, Button, ButtonGroup, FooterLinks };
